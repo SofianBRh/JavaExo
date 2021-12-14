b52221b84ed43410d0adfa306dd918a7d099ebb7
SofianBRh <95184450+SofianBRh@users.noreply.github.com>
2021-12-14 15:15:42 +0100

    Add files via upload

---
+++ IndexBoucle.js
@@ -0,0 +1,60 @@
+for(var i=0; i<10; i=i+1){
+console.log(i);}
+//exo2
+var a ;
+var b;
+a=0;
+b=5;
+if(1< a < 100){
+    while(b < 20){
+        b = b * a ;
+        console.log(b);
+        b ++;
+    }
+}
+//exo 3 
+
+var d = '100';
+var e ='1';
+if(1<e<100){
+    while(d>=10){
+        d = d * e ;
+        d--;
+        console.log(d);
+    }
+}
+
+//exo4
+
+var g = '1';
+while(g<10){
+console.log(g);
+g= g *1.5;}
+
+//exo05
+var q = 1;
+while( q < 15) {
+    console.log("on arrive quand");
+    q++ ;
+    }
+
+//exo 06 
+var x = 20 ;
+while(x>0){
+    console.log("c'est presque bon");
+    x--;
+}
+
+   //exo7
+    var s = 1;
+    while(s <100) {
+        console.log("on tient le bout");
+        s = s + 15;
+    }
+
+    //exo8
+    var pj= 200;
+    while(pj>0){
+        console.log('Enfin!!!');
+        pj = pj - 12;
+    }
